87d456f48e90f97cdef5c85e848bdffca6383c6a
damage99 <p_prostoj@mail.ru>
2017-10-16 19:42:09 +0400

    bots: correct arguments check and dont allow invalid ones
    -From Voron00

--- gsc_bots.cpp
+++ gsc_bots.cpp
@@ -13,17 +13,33 @@ void gsc_bots_set_walkdir(int id)
 		return;
 	}
 
+	if (ADDRESSTYPE(id) != NA_BOT)
+	{
+		stackError("gsc_bots_set_walkdir() player %i is not a bot", id);
+		stackPushUndefined();
+		return;
+	}
+
 	extern int bot_movement[64];
-	if(!strcmp(dir, "none"))
+
+	if (strcmp(dir, "none") == 0)
 		bot_movement[id] = (0);
-	else if(!strcmp(dir, "forward"))
+	else if (strcmp(dir, "forward") == 0)
 		bot_movement[id] = (127);
-	else if(!strcmp(dir, "back"))
+	else if (strcmp(dir, "back") == 0)
 		bot_movement[id] = (129);
-	else if(!strcmp(dir, "right"))
+	else if (strcmp(dir, "right") == 0)
 		bot_movement[id] = (127 << 8);
-	else if(!strcmp(dir, "left"))
+	else if (strcmp(dir, "left") == 0)
 		bot_movement[id] = (129 << 8);
+	else
+	{
+		stackError("gsc_bots_set_walkdir() invalid argument '%s'. Valid arguments are: 'none' 'forward' 'back' 'right' 'left'", dir);
+		stackPushUndefined();
+		return;
+	}
+
+	stackPushInt(1);
 }
 
 void gsc_bots_set_lean(int id)
@@ -37,13 +53,29 @@ void gsc_bots_set_lean(int id)
 		return;
 	}
 
+	if (ADDRESSTYPE(id) != NA_BOT)
+	{
+		stackError("gsc_bots_set_lean() player %i is not a bot", id);
+		stackPushUndefined();
+		return;
+	}
+
 	extern int bot_lean[64];
-	if(!strcmp(lean, "none"))
+
+	if (strcmp(lean, "none") == 0)
 		bot_lean[id] = (0);
-	else if(!strcmp(lean, "left"))
+	else if (strcmp(lean, "left") == 0)
 		bot_lean[id] = (64);
-	else if(!strcmp(lean, "right"))
+	else if (strcmp(lean, "right") == 0)
 		bot_lean[id] = (128);
+	else
+	{
+		stackError("gsc_bots_set_lean() invalid argument '%s'. Valid arguments are: 'right' 'left'", lean);
+		stackPushUndefined();
+		return;
+	}
+
+	stackPushInt(1);
 }
 
 void gsc_bots_set_stance(int id)
@@ -57,15 +89,31 @@ void gsc_bots_set_stance(int id)
 		return;
 	}
 
+	if (ADDRESSTYPE(id) != NA_BOT)
+	{
+		stackError("gsc_bots_set_stance() player %i is not a bot", id);
+		stackPushUndefined();
+		return;
+	}
+
 	extern int bot_stance[64];
-	if(!strcmp(stance, "stand"))
+
+	if (strcmp(stance, "stand") == 0)
 		bot_stance[id] = (0);
-	else if(!strcmp(stance, "crouch"))
+	else if (strcmp(stance, "crouch") == 0)
 		bot_stance[id] = (512);
-	else if(!strcmp(stance, "prone"))
+	else if (strcmp(stance, "prone") == 0)
 		bot_stance[id] = (256);
-	else if(!strcmp(stance, "jump"))
+	else if (strcmp(stance, "jump") == 0)
 		bot_stance[id] = (1024);
+	else
+	{
+		stackError("gsc_bots_set_stance() invalid argument '%s'. Valid arguments are: 'stand' 'crouch' 'prone' 'jump'", stance);
+		stackPushUndefined();
+		return;
+	}
+
+	stackPushInt(1);
 }
 
 void gsc_bots_thrownade(int id)
@@ -79,11 +127,21 @@ void gsc_bots_thrownade(int id)
 		return;
 	}
 
+	if (ADDRESSTYPE(id) != NA_BOT)
+	{
+		stackError("gsc_bots_thrownade() player %i is not a bot", id);
+		stackPushUndefined();
+		return;
+	}
+
 	extern int bot_grenade[64];
+
 	if (!grenade)
 		bot_grenade[id] = (0);
 	else
 		bot_grenade[id] = (65536);
+
+	stackPushInt(1);
 }
 
 void gsc_bots_fireweapon(int id)
@@ -97,11 +155,21 @@ void gsc_bots_fireweapon(int id)
 		return;
 	}
 
+	if (ADDRESSTYPE(id) != NA_BOT)
+	{
+		stackError("gsc_bots_fireweapon() player %i is not a bot", id);
+		stackPushUndefined();
+		return;
+	}
+
 	extern int bot_shoot[64];
+
 	if (!shoot)
 		bot_shoot[id] = (0);
 	else
 		bot_shoot[id] = (1);
+
+	stackPushInt(1);
 }
 
 void gsc_bots_meleeweapon(int id)
@@ -115,11 +183,21 @@ void gsc_bots_meleeweapon(int id)
 		return;
 	}
 
+	if (ADDRESSTYPE(id) != NA_BOT)
+	{
+		stackError("gsc_bots_meleeweapon() player %i is not a bot", id);
+		stackPushUndefined();
+		return;
+	}
+
 	extern int bot_melee[64];
+
 	if (!melee)
 		bot_melee[id] = (0);
 	else
 		bot_melee[id] = (32772);
+
+	stackPushInt(1);
 }
 
 void gsc_bots_reloadweapon(int id)
@@ -133,11 +211,21 @@ void gsc_bots_reloadweapon(int id)
 		return;
 	}
 
+	if (ADDRESSTYPE(id) != NA_BOT)
+	{
+		stackError("gsc_bots_reloadweapon() player %i is not a bot", id);
+		stackPushUndefined();
+		return;
+	}
+
 	extern int bot_reload[64];
+
 	if (!reload)
 		bot_reload[id] = (0);
 	else
 		bot_reload[id] = (16);
+
+	stackPushInt(1);
 }
 
 void gsc_bots_adsaim(int id)
@@ -151,11 +239,21 @@ void gsc_bots_adsaim(int id)
 		return;
 	}
 
+	if (ADDRESSTYPE(id) != NA_BOT)
+	{
+		stackError("gsc_bots_adsaim() player %i is not a bot", id);
+		stackPushUndefined();
+		return;
+	}
+
 	extern int bot_ads[64];
+
 	if (!ads)
 		bot_ads[id] = (0);
 	else
 		bot_ads[id] = (4096);
+
+	stackPushInt(1);
 }
 
 void gsc_bots_switchtoweaponid(int id)
@@ -169,8 +267,17 @@ void gsc_bots_switchtoweaponid(int id)
 		return;
 	}
 
+	if (ADDRESSTYPE(id) != NA_BOT)
+	{
+		stackError("gsc_bots_switchtoweaponid() player %i is not a bot", id);
+		stackPushUndefined();
+		return;
+	}
+
 	extern int bot_weapon[64];
+
 	bot_weapon[id] = (weaponid);
+	stackPushInt(1);
 }
 
 #endif
