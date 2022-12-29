927bda6d9a4d8947e4396806d45dee322c1cb65c
iBuddie <43479204+ibuddieat@users.noreply.github.com>
2022-12-29 05:06:36 +0100

    moved file existance check in loadSoundFile

--- code/gsc_utils.cpp
+++ code/gsc_utils.cpp
@@ -1033,97 +1033,91 @@ void *encode_async(void *newtask)
 	encoder_async_task *task = (encoder_async_task*)newtask;
 	int result = 0;
 
+	// Reset sound data for this slot
+	memset(&voiceDataStore[task->soundIndex - 1], 0, sizeof(voiceDataStore[0]));
+
+	/*
+	The following encoding procedure is based on:
+	https://gitlab.xiph.org/xiph/speex/-/blob/Speex-1.1.9/doc/sampleenc.c
+
+	License:
+	Copyright 2002-2004 Xiph.org Foundation, Jean-Marc Valin, David Rowe, EpicGames
+
+	Redistribution and use in source and binary forms, with or without
+	modification, are permitted provided that the following conditions
+	are met:
+
+	- Redistributions of source code must retain the above copyright
+	notice, this list of conditions and the following disclaimer.
+
+	- Redistributions in binary form must reproduce the above copyright
+	notice, this list of conditions and the following disclaimer in the
+	documentation and/or other materials provided with the distribution.
+
+	- Neither the name of the Xiph.org Foundation nor the names of its
+	contributors may be used to endorse or promote products derived from
+	this software without specific prior written permission.
+
+	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+	``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+	LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+	A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
+	CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+	EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+	PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+	PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+	LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+	NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+	SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+	*/
+	short in[MAX_VOICEFRAMESIZE];
+	float input[MAX_VOICEFRAMESIZE];
+	char data[MAX_VOICEPACKETDATALEN];
+	int dataLen;
+	void *g_encoder;
+	SpeexBits encodeBits;
+	int i, packetIndex;
+	VoicePacket_t *voicePacket;
+
+	// (Re)Open input file
 	FILE *file = fopen(task->filePath, "r");
-	if ( file != NULL )    
+
+	// Create a new encoder state in narrowband mode
+	g_encoder = speex_encoder_init(&speex_nb_mode);
+	speex_bits_init(&encodeBits);
+	Encode_SetOptions(g_encoder);
+
+	for ( packetIndex = 0; packetIndex <= MAX_STOREDVOICEPACKETS; packetIndex++ )
 	{
-		// Reset sound data for this slot
-		memset(&voiceDataStore[task->soundIndex - 1], 0, sizeof(voiceDataStore[0]));
-
-		/*
-		The following encoding procedure is based on:
-		https://gitlab.xiph.org/xiph/speex/-/blob/Speex-1.1.9/doc/sampleenc.c
-
-		License:
-		Copyright 2002-2004 Xiph.org Foundation, Jean-Marc Valin, David Rowe, EpicGames
-
-		Redistribution and use in source and binary forms, with or without
-		modification, are permitted provided that the following conditions
-		are met:
-
-		- Redistributions of source code must retain the above copyright
-		notice, this list of conditions and the following disclaimer.
-
-		- Redistributions in binary form must reproduce the above copyright
-		notice, this list of conditions and the following disclaimer in the
-		documentation and/or other materials provided with the distribution.
-
-		- Neither the name of the Xiph.org Foundation nor the names of its
-		contributors may be used to endorse or promote products derived from
-		this software without specific prior written permission.
-
-		THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-		``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-		LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-		A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE FOUNDATION OR
-		CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-		EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-		PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-		PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-		LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-		NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-		SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-		*/
-		short in[MAX_VOICEFRAMESIZE];
-		float input[MAX_VOICEFRAMESIZE];
-		char data[MAX_VOICEPACKETDATALEN];
-		int dataLen;
-		void *g_encoder;
-		SpeexBits encodeBits;
-		int i, packetIndex;
-		VoicePacket_t *voicePacket;
-
-		// Create a new encoder state in narrowband mode
-		g_encoder = speex_encoder_init(&speex_nb_mode);
-		speex_bits_init(&encodeBits);
-		Encode_SetOptions(g_encoder);
-
-		for ( packetIndex = 0; packetIndex <= MAX_STOREDVOICEPACKETS; packetIndex++ )
+		if ( packetIndex == MAX_STOREDVOICEPACKETS )
 		{
-			if ( packetIndex == MAX_STOREDVOICEPACKETS )
-			{
-				// Sound file too long, end encoding here
-				result = 1;
-				break;
-			}
-
-			// Read a 16 bits/sample audio frame
-			fread(in, sizeof(short), MAX_VOICEFRAMESIZE, file);
-			if ( feof(file) )
-				break;
-
-			for ( i = 0; i < MAX_VOICEFRAMESIZE; i++ )
-				input[i] = in[i] * task->volume;
-
-			speex_bits_reset(&encodeBits);
-			speex_encode(g_encoder, input, &encodeBits);
-			dataLen = speex_bits_write(&encodeBits, data, MAX_VOICEPACKETDATALEN);
-			voicePacket = &voiceDataStore[task->soundIndex - 1][packetIndex];
-			memcpy(voicePacket->data, data, dataLen);
-			voicePacket->dataLen = dataLen;
+			// Sound file too long, end encoding here
+			result = 1;
+			break;
 		}
-		// Reset subsequent voice packet (in case a long sound is replaced by a shorter one)
-		if ( packetIndex != MAX_STOREDVOICEPACKETS )
-			memset(&voiceDataStore[task->soundIndex - 1][packetIndex], 0, sizeof(voiceDataStore[0][0]));
 
-		speex_encoder_destroy(g_encoder);
-		speex_bits_destroy(&encodeBits);
-		fclose(file);
+		// Read a 16 bits/sample audio frame
+		fread(in, sizeof(short), MAX_VOICEFRAMESIZE, file);
+		if ( feof(file) )
+			break;
+
+		for ( i = 0; i < MAX_VOICEFRAMESIZE; i++ )
+			input[i] = in[i] * task->volume;
+
+		speex_bits_reset(&encodeBits);
+		speex_encode(g_encoder, input, &encodeBits);
+		dataLen = speex_bits_write(&encodeBits, data, MAX_VOICEPACKETDATALEN);
+		voicePacket = &voiceDataStore[task->soundIndex - 1][packetIndex];
+		memcpy(voicePacket->data, data, dataLen);
+		voicePacket->dataLen = dataLen;
 	}
-	else
-	{
-		stackError("gsc_utils_loadsoundfile() input file could not be opened");
-		result = 2;
-	}
+	// Reset subsequent voice packet (in case a long sound is replaced by a shorter one)
+	if ( packetIndex != MAX_STOREDVOICEPACKETS )
+		memset(&voiceDataStore[task->soundIndex - 1][packetIndex], 0, sizeof(voiceDataStore[0][0]));
+
+	speex_encoder_destroy(g_encoder);
+	speex_bits_destroy(&encodeBits);
+	fclose(file);
 
 	if ( Scr_IsSystemActive() )
 	{
@@ -1262,7 +1256,19 @@ void gsc_utils_loadsoundfile()
 		current->next = newtask;
 	else
 		first_encoder_async_task = newtask;
-	
+
+	FILE *file = fopen(filePath, "r");
+	if ( !file )
+	{
+		stackError("gsc_utils_loadsoundfile() input file could not be opened");
+		stackPushUndefined();
+		return;
+	}
+	else
+	{
+		fclose(file);
+	}
+
 	pthread_t encoder_doer;
 
 	if ( pthread_create(&encoder_doer, NULL, encode_async, newtask) != 0 )
