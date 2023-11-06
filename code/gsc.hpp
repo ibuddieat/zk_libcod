e5baff062936de22a0a03a15e600a5ef83d02d94
iBuddie <43479204+ibuddieat@users.noreply.github.com>
2023-11-06 17:36:17 +0100

    fixed copy and paste error

--- code/gsc.hpp
+++ code/gsc.hpp
@@ -123,7 +123,7 @@ void Scr_AddFunc(const char *value);
 int Q_vsnprintf(char *s0, size_t size, const char *fmt, va_list args);
 #endif
 
-#ifndef str_iseq
+#ifndef strcmp_constant_time
 int strcmp_constant_time(const char *s1, const char *s2);
 #endif
 
